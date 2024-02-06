6771e41277c74760f01da82f9d305f2c7ec5c839
Alexander Han <boikoale97@gmail.com>
2024-02-06 21:55:45 +0200

    fix

--- src/main/resources/static/js/header.module.mjs
+++ src/main/resources/static/js/header.module.mjs
@@ -1,6 +1,6 @@
 export function headerModule() {
     const userEmail = document.querySelector(".logout-form__email");
-    let isNotAuth = userEmail.textContent === "anonymousUser";
+    const isNotAuth = userEmail.textContent === "anonymousUser";
     const logoutButton = document.querySelector(".logout-form__button");
     const profileButton = document.querySelector(".button__profile");
 
