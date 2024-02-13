6df1a6f88fec29f2380c74b0785bd442ed73ad12
user <user@MacBook-Pro-user.local>
2024-02-13 23:28:41 +0200

    registration with blunk email fixed

--- src/main/java/com/novo/personalproject/dto/UserCreateEditDto.java
+++ src/main/java/com/novo/personalproject/dto/UserCreateEditDto.java
@@ -5,6 +5,7 @@ import com.novo.personalproject.model.entity.Order;
 import com.novo.personalproject.model.entity.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Value;
 
@@ -16,9 +17,12 @@ import java.util.List;
 @Value
 @Builder
 public class UserCreateEditDto {
+    @NotBlank(message = "Email is required")
+    @NotNull(message = "Email is required")
     @Email(message = "Invalid email address")
     String email;
 
+    @NotNull(message = "Password is required")
     @NotBlank(message = "Password is required")
     String password;
 
