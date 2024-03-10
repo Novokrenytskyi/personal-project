eed570865be3dcb63fb1807a0a2eda5e92cb6863
Alexander Han <73641551+srcHan-u@users.noreply.github.com>
2024-03-10 16:51:47 +0200

    Implement product api (#21)
    * Implement product feature enhancements and fixes
    
       -  Updated product management components (controller, service, DTO).
       -  Adjusted security configuration.
       -  Modified application properties.
       -  Refined CSS styling.
       -  Improved JavaScript functionality for product page and utilities.
       -  Updated product page HTML template.
    
    * Fix

---
+++ src/main/resources/static/js/product.page.js
@@ -0,0 +1,111 @@
+import "./loader.module.js";
+import "./header.module.js";
+import {showNotification} from "./utils.js";
+
+const form = document.querySelector('.product-form');
+const imagePreview = document.getElementById("image-preview");
+
+const inputName = document.querySelector('#name');
+const inputDescription = document.querySelector('#description');
+const inputImage = document.querySelector('#image');
+const inputProductType = document.querySelector('#productType');
+const inputPrice = document.querySelector('#price');
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const imageFile = inputImage.files[0];
+    if (!imageFile.type.startsWith('image/')) {
+        showNotification("Please select an image file", "error");
+        return;
+    }
+
+    if (imageFile.size > 5 * 1024 * 1024) { // 5 MB limit
+        showNotification("Image file size too large", "error");
+        return;
+    }
+
+
+    const product = {
+        name: inputName.value,
+        description: inputDescription.value,
+        image: imageFile,
+        productType: inputProductType.value,
+        price: Number(inputPrice.value),
+    };
+
+    try {
+        const formData = new FormData();
+
+        for (const key in product) {
+            formData.append(key, product[key]);
+        }
+
+        const response = await submitForm(API_URL, formData);
+        if (response.ok) {
+            showNotification("Product created successfully!", "success");
+        } else {
+            throw new Error('Error creating product');
+        }
+    } catch (error) {
+        showNotification(error.message, "error");
+    } finally {
+        clearForm();
+    }
+});
+
+async function submitForm(url, data) {
+    const config = {
+        method: 'POST',
+        body: data,
+    };
+
+    try {
+        document.body.classList.add("load");
+        const response = await fetch(url, config);
+        document.body.classList.remove("load");
+        return response;
+    } catch (error) {
+        throw new Error('Error submitting form');
+    }
+
+}
+
+inputImage.addEventListener("change", () => {
+    const file = this.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        reader.addEventListener("load", function() {
+            imagePreview.src = this.result;
+            imagePreview.style.display = "block";
+        });
+
+        reader.readAsDataURL(file);
+    } else {
+        imagePreview.src = "";
+        imagePreview.style.display = "none";
+    }
+});
+
+function clearForm() {
+    inputName.value = '';
+    inputDescription.value = '';
+    inputImage.value = '';
+    inputProductType.selectedIndex = 0;
+    inputPrice.value = '';
+}
+
+inputPrice.addEventListener("input", () => {
+    const isValid = this.value.match(/^\d\.*/g);
+
+    if(!isValid) {
+        this.value = "";
+        showNotification("Enter only the numbers ", "error");
+    }
+
+});
+
+const API_URL = 'http://localhost:8080/api/products';
+
--- src/main/resources/static/js/utils.js
+++ src/main/resources/static/js/utils.js
@@ -17,6 +17,28 @@ export function fetchData(url) {
         });
 }
 
+
 export function moveTo(url) {
     window.location.href = url;
+}
+
+
+export function showNotification(text, status) {
+    const notificationElement = document.querySelector('.notification');
+    const notificationText = document.querySelector('.notification__text');
+
+    notificationText.textContent = text;
+    notificationElement.classList.add(status ?? "");
+    notificationElement.classList.remove('hidden');
+
+
+    notificationElement.style.animation = 'notification-appear 1s ease-in-out';
+
+    setTimeout(() => {
+        notificationElement.classList.add('hidden');
+        notificationElement.classList.remove(status);
+        notificationText.textContent = '';
+        notificationElement.style.animation = '';
+    }, 3000);
+
 }
